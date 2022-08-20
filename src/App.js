c3ff613df6116e0181ced8eea226fce7c013f97d
Vipul Kumar Singh <vipul24.9669@gmail.com>
2022-08-20 18:31:04 +0530

    Added react-dotenv

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import classes from './App.module.css';
 
 function App() {
+  const onClickHandler = () => {
+    console.log(`${process.env.REACT_APP_MY_KEY}`);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={onClickHandler} className={classes.button}>Click Here</button>
     </div>
   );
 }
